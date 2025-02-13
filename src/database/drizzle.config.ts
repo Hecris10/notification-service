b07c96b9ca1ad137492a3bff1fca460f38b9ad06
Helamã Ewerton <helamaewerton@gmail.com>
2025-02-12 20:55:48 -0700

    remove unused database configuration and notification schema files

--- src/database/drizzle.config.ts
+++
@@ -1,7 +0,0 @@
-// src/database/drizzle.config.ts
-import { drizzle } from 'drizzle-orm/sqlite3';
-import { Database } from 'sqlite3';
-import * as schema from './schemas/notification.schema';
-
-const sqlite = new Database('./notifications.db');
-export const db = drizzle(sqlite, { schema });
